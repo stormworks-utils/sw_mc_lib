54140f2728ec8ca7163beb7a831a37798ff9f462
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-22 08:24:35 +0200

    added xml formatter

---
+++ sw_mc_lib/XMLFormatter.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+from .XMLParser import XMLParserElement
+
+
+def _escape_string(to_escape: str) -> str:
+    """
+    Escapes a string so that it results in valid xml. To be used like following:
+
+    `abc<>"def\n"` -> `'abc&lt;&gt;"def\n"'`
+    """
+    to_escape = to_escape.replace("&", "&amp;")
+    to_escape = to_escape.replace("<", "&lt;")
+    to_escape = to_escape.replace(">", "&gt;")
+    if to_escape.count('"') > to_escape.count("'"):
+        to_escape = to_escape.replace("'", "&apos;")
+        to_escape = f"'{to_escape}'"
+    else:
+        to_escape = to_escape.replace('"', "&quot;")
+        to_escape = f'"{to_escape}"'
+    return to_escape
+
+
+def _indent(to_indent: str, indentation_character: str, line_breaks: str) -> str:
+    """
+    Indents the string by one tab and adds a newline at the end
+
+    `<node/>` -> `\t<node/>\n`
+    """
+    return (
+        "\n".join(
+            indentation_character + line for line in to_indent.rstrip().split("\n")
+        )
+        + line_breaks
+    )
+
+
+def format(element: XMLParserElement, indentation: Optional[str] = "\t") -> str:
+    """
+    Formats a XMLParserElement into a xml string. If indentation is None, there will be no line breaks or indentation.
+    """
+    line_breaks: str = "\n" if indentation is not None else ""
+    indentation_character = indentation or ""
+    xml: str = f"<{element.tag}"
+    for name, value in element.attributes.items():
+        xml += f" {name}={_escape_string(value)}"
+    if element.children:
+        xml += f">{line_breaks}"
+        for child in element.children:
+            xml += _indent(
+                format(child, indentation), indentation_character, line_breaks
+            )
+        xml += f"</{element.tag}>{line_breaks}"
+    else:
+        xml += f"/>{line_breaks}"
+    return xml
