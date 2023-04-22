c88a073bed3f4ca6d5df3e26a8b635b7830267fa
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-22 08:31:37 +0200

    added the possibility to include a xml header in the generated output

--- sw_mc_lib/XMLFormatter.py
+++ sw_mc_lib/XMLFormatter.py
@@ -35,13 +35,15 @@ def _indent(to_indent: str, indentation_character: str, line_breaks: str) -> str
     )
 
 
-def format(element: XMLParserElement, indentation: Optional[str] = "\t") -> str:
+def format(element: XMLParserElement, indentation: Optional[str] = "\t", header: bool = False) -> str:
     """
     Formats a XMLParserElement into a xml string. If indentation is None, there will be no line breaks or indentation.
+    If header is true, it will add a xml declaration at the top of the document
     """
     line_breaks: str = "\n" if indentation is not None else ""
     indentation_character = indentation or ""
-    xml: str = f"<{element.tag}"
+    xml: str = f'<?xml version="1.0" encoding="UTF-8"?>{line_breaks}' if header else ''
+    xml += f"<{element.tag}"
     for name, value in element.attributes.items():
         xml += f" {name}={_escape_string(value)}"
     if element.children:
