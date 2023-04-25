a4218a8dff9b3d7946b20d605770a0246775b98e
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-25 23:24:16 +0200

    added __eq__ to XMLElement

--- sw_mc_lib/XMLElement.py
+++ sw_mc_lib/XMLElement.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import Generator, Any
 
 from .XMLParser import XMLParserElement
 from .util import generic_str, string_to_sw_float
@@ -23,3 +24,14 @@ class XMLElement(ABC):
 
     def __repr__(self) -> str:
         return generic_str(self)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return all(
+            callable(getattr(self, i)) or getattr(self, i) == getattr(other, i)
+            for i in self.__dir()
+        )
+
+    def __dir(self) -> Generator[str, None, None]:
+        return (i for i in self.__dir__() if not i.startswith("__"))
