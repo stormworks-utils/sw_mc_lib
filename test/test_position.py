ddbd2b3385aec371a49b52f0cf83f337ec3c6a27
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-04-30 10:03:44 +0200

    tests for position

---
+++ test/test_position.py
@@ -0,0 +1,30 @@
+import unittest
+
+from sw_mc_lib.Position import Position, XMLParserElement
+
+
+class TestPosition(unittest.TestCase):
+    def test_from_xml(self) -> None:
+        elem: XMLParserElement = XMLParserElement("pos")
+        expected: Position = Position(0, 0)
+        self.assertEqual(Position.from_xml(elem), expected)
+        elem.attributes["x"] = "1.25"
+        expected.x = 1.25
+        self.assertEqual(Position.from_xml(elem), expected)
+        elem.attributes["y"] = "-4.75"
+        expected.y = -4.75
+        self.assertEqual(Position.from_xml(elem), expected)
+
+    def test_empty_pos(self) -> None:
+        self.assertEqual(Position.empty_pos(), Position(0, 0))
+
+    def test_to_xml(self) -> None:
+        elem: Position = Position(0, 0)
+        expected: XMLParserElement = XMLParserElement("pos")
+        self.assertEqual(elem.to_xml(), expected)
+        elem.x = 1.25
+        expected.attributes["x"] = "1.25"
+        self.assertEqual(elem.to_xml(), expected)
+        elem.y = -4.0
+        expected.attributes["y"] = "-4.0"
+        self.assertEqual(elem.to_xml(), expected)
